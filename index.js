49f56207e935cdd4b3dcb9960390edda03ef6739
bhargav yogesh <bhargav.yogesh@gmail.com>
2020-03-16 21:38:38 -0400

    actual first commit

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const env = require('dotenv').config()
+
+if(env.error){
+    console.log(env.error);
+    throw env.error;
+}
 twitter = require('./src/catchers/twitter');
 api = require('./src/rest-api');
--- src/catchers/twitter.js
+++ src/catchers/twitter.js
@@ -1,11 +1,11 @@
 const Twitter = require('twitter');
-const req_api = require('../req-api')
- 
+const req_api = require('../req-api');
+
 var client = new Twitter({
-  consumer_key: 'ON9R4sHi80lsYPoggT86SIZt8',
-  consumer_secret: 'ItOHfVl2XxHKNCGSNNNDeQGLRWza5NCJNiVu9Y6ANDF2d4y07M',
-  access_token_key: '2759958727-Tqi0Cf7NmzvWYYmAeag7fEHcXy6Fn1t0oVf1XcO',
-  access_token_secret: 'dJR8Ll3awFCV6cnuaB5u8o6LH04aUfsOdWWzsM9QaTWSV'
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_secret
 });
  
 client.stream('statuses/filter', {follow: '2759958727'}, function(stream) {
